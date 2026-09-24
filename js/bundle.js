0ed654c66a4b4307a4fa98084039d91d326b6264
Natthawut <sunsmile872@gmail.com>
2026-09-24 19:50:06 +0700

    auto-backup: 9/24/2026, 7:50:06 PM (1 files updated)

--- js/bundle.js
+++ js/bundle.js
@@ -2367,7 +2367,10 @@
           break;
         case "joint_focus":
           if (targetJointPos) {
-            targetPos.copy(targetJointPos).add(new THREE.Vector3(0.5, 0.2, 1.1));
+            const isFoot = targetJointPos.y < 0.35;
+            const isHand = targetJointPos.y < 1.25 && targetJointPos.x > 0.12;
+            const offset = isFoot || isHand ? new THREE.Vector3(0.2, 0.1, 0.42) : new THREE.Vector3(0.5, 0.2, 1.1);
+            targetPos.copy(targetJointPos).add(offset);
             newTarget.copy(targetJointPos);
           } else {
             targetPos.set(0.6, 1.35, 1.2);
